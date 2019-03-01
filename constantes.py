e8299678cf2d7be8fe861438f95a70c30cd64695
helenecourau <helene.courau@gmail.com>
2019-03-01 17:04:08 +0100

    add content constantes.py and initiate macgyver.py

---
+++ constantes.py
@@ -0,0 +1,18 @@
+"""Constantes du jeu du Labyrinthe de MacGyver"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "MacGyver"
+
+#Listes des images du jeu
+image_fond = "images/fond.jpg"
+image_mur = "images/mur.png"
+image_macgyver = "images/MacGyver.png"
+image_gardien = "images/Gardien.png"
+image_aiguille = "images/aiguille.png"
+image_ether = "images/ether.png"
+image_seringue = "images/seringue.png"
