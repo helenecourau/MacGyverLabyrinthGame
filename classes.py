c40dc89b8a7e8ef934e5a606cd60b8af89dfc0c5
helenecourau <helene.courau@gmail.com>
2019-03-01 18:24:15 +0100

    changes in classes.py

--- classes.py
+++ classes.py
@@ -8,3 +8,7 @@ class Map:
 	def __init__(self, fichier_lab):
 		self.fichier_lab = fichier_lab
 		self.structure = 0
+
+	def generer(self):
+		"Générer la map en fonction du fichier"
+		with open(labyrinth.txt, "r") as fichier_lab
